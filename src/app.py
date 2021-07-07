d39101a2b42fcb1ac28e314a249db5b03cc6b5fb
fernandor-p <fernandor.p@hotmail.com>
2021-07-07 18:51:12 -0400

    initial family members added

--- src/app.py
+++ src/app.py
@@ -30,12 +30,7 @@ def handle_hello():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
-
-
+    response_body = members
     return jsonify(response_body), 200
 
 # this only runs if `$ python src/app.py` is executed
--- src/datastructures.py
+++ src/datastructures.py
@@ -13,7 +13,25 @@ class FamilyStructure:
         self.last_name = last_name
 
         # example list of members
-        self._members = []
+        self._members = [{
+            "id": self._generateId(),
+            "first_name": "John",
+            "last_name": "Jackson",
+            "years": 33,
+            "lucky_numbers": [7,13,22]
+        },{
+            "id": self._generateId(),
+            "first_name": "Jane",
+            "last_name": "Jackson",
+            "years": 35,
+            "lucky_numbers": [10,14,3]
+        } ,{
+            "id": self._generateId(),
+            "first_name": "Jimmy",
+            "last_name": "Jackson",
+            "years": 5,
+            "lucky_numbers": [1]
+        }]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
@@ -21,6 +39,7 @@ class FamilyStructure:
 
     def add_member(self, member):
         # fill this method and update the return
+        
         pass
 
     def delete_member(self, id):
